8c47b82153c05cf046d1341627bb120795248056
arispujud <ariz.pk@gmail.com>
2021-02-18 16:09:37 +0700

    first commit

---
+++ kedali_motor.ino
@@ -0,0 +1,24 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  putarKiri();
+  delay(1000);
+  putarKanan();
+  delay(1000);
+}
+
+void putarKiri(){
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+}
+void putarKanan(){
+  digitalWrite(13, HIGH);
+  digitalWrite(12, LOW);
+}
+
